7f42f4f00755a8061ff88a1f37e2eeb8e86bdf67
vineethreddy2000 <145160293+vineethreddy2000@users.noreply.github.com>
2024-03-25 17:51:24 -0400

    Update Views.sql

--- SQLFiles/Views.sql
+++ SQLFiles/Views.sql
@@ -1,72 +1,145 @@
 
---List of Active Properties with Lister Details
-CREATE VIEW ActivePropertiesWithLister AS
-SELECT P.Property_ID, P.Address, P.Price, P.Property_Type,
-       L.Lister_ID, L.Lister_Type, L.Active_Properties,
-       Ls.Listing_ID, Ls.Listing_Date, Ls.Status
-FROM Property P
-JOIN Listing Ls ON P.Property_ID = Ls.Property_ID
-JOIN Lister L ON Ls.Lister_ID = L.Lister_ID
-WHERE Ls.Status = 'Active';
-
---Listers with Most Active Properties:
-CREATE VIEW TopActiveListers AS
-SELECT L.Lister_ID, L.Lister_Type, L.Active_Properties,
-       U.First_Name + ' ' + U.Last_Name AS Lister_Name
-FROM Lister L
-JOIN [User] U ON L.Lister_ID = U.User_ID
-ORDER BY L.Active_Properties DESC;
-
-
---Monthly Listing Trends:
-CREATE VIEW MonthlyListingTrends AS
-SELECT MONTH(Ls.Listing_Date) AS Listing_Month,
-       YEAR(Ls.Listing_Date) AS Listing_Year,
-       COUNT(*) AS Total_Listings
-FROM Listing Ls
-GROUP BY MONTH(Ls.Listing_Date), YEAR(Ls.Listing_Date)
-ORDER BY Listing_Year, Listing_Month;
+-- Active Properties with Details
+CREATE VIEW ActivePropertyDetails AS
+SELECT
+    P.Property_ID,
+    P.Address_Line_1,
+    P.Address_Line_2,
+    P.City,
+    P.State,
+    P.Zipcode,
+    PD.Bedroom,
+    PD.Bathroom,
+    PD.Area_Size,
+    L.Lister_ID,
+    L.Lister_Type,
+    L.Active_Properties
+FROM
+    Property AS P
+JOIN
+    Property_Detail AS PD ON P.Property_ID = PD.Property_ID
+JOIN
+    Lister AS L ON P.Lister_ID = L.Lister_ID
+JOIN
+    Listing AS LI ON P.Property_ID = LI.Property_ID
+WHERE
+    LI.Listing_Status = 'Active';
 
 
---Properties with Price Range Categories:
-CREATE VIEW PriceRangeProperties AS
-SELECT Property_ID, Address, Price,
-       CASE
-           WHEN Price < 100000 THEN 'Low Range'
-           WHEN Price BETWEEN 100000 AND 300000 THEN 'Medium Range'
-           WHEN Price > 300000 THEN 'High Range'
-       END AS Price_Category
-FROM Property;
+	select * from ActivePropertyDetails
 
 
---Recently Sold Properties:
-CREATE VIEW RecentlySoldProperties AS
-SELECT P.Property_ID, P.Address, P.Price,
-       L.Lister_ID, L.Lister_Type,
-       Ls.Listing_ID, Ls.Listing_Date, Ls.Status
-FROM Property P
-JOIN Listing Ls ON P.Property_ID = Ls.Property_ID
-JOIN Lister L ON Ls.Lister_ID = L.Lister_ID
-WHERE Ls.Status = 'Sold';
+	--Customer Messages with Property Details
+	CREATE VIEW CustomerPropertyMessages AS
+SELECT
+    M.Message_ID,
+    M.Customer_ID,
+    M.Lister_ID,
+    M.Subject,
+    M.Content,
+    P.Address_Line_1,
+    P.City,
+    P.State,
+    P.Zipcode
+FROM
+    [Message] AS M
+JOIN
+    Property AS P ON M.Lister_ID = P.Lister_ID;
 
---Average Price per Property Type
-CREATE VIEW AveragePricePerType AS
-SELECT Property_Type, AVG(Price) AS Avg_Price
-FROM Property
-GROUP BY Property_Type;
-
-
---Listers with No Active Properties:
-CREATE VIEW ListersWithNoActiveProperties AS
-SELECT L.Lister_ID, L.Lister_Type, L.Active_Properties,
-       U.First_Name + ' ' + U.Last_Name AS Lister_Name
-FROM Lister L
-JOIN [User] U ON L.Lister_ID = U.User_ID
-WHERE L.Active_Properties = 0;
+	Select * from CustomerPropertyMessages
 
 
---Properties with More Bathrooms than Bedrooms
-CREATE VIEW PropertiesMoreBathrooms AS
-SELECT Property_ID, Address, Bedrooms, Bathrooms
-FROM Property
-WHERE Bathrooms > Bedrooms;
+	
+  --Property Details with Amenities
+
+  CREATE VIEW Property_Amenities AS
+SELECT
+    P.Property_ID,
+    P.Address_Line_1,
+    P.Address_Line_2,
+    P.City,
+    P.State,
+    P.Zipcode,
+    PD.Bedroom,
+    PD.Bathroom,
+    PD.Area_Size,
+    A.Water,
+    A.Gas,
+    A.Electricity,
+    A.Parking,
+    A.Pets_Allowed
+FROM
+    Property P
+INNER JOIN
+    Property_Detail PD ON P.Property_ID = PD.Property_ID
+INNER JOIN
+    Listing L ON P.Property_ID = L.Property_ID
+INNER JOIN
+    Amenity A ON L.Listing_ID = A.Listing_ID;
+
+	select * from Property_Amenities
+
+
+	--Customer Reviews and Ratings
+
+	CREATE VIEW Customer_Reviews AS
+SELECT
+    R.Review_ID,
+    U.First_Name AS Customer_FirstName,
+    U.Last_Name AS Customer_LastName,
+    U.Email AS Customer_Email,
+    U2.User_ID AS Lister_ID,
+    U2.First_Name AS Lister_FirstName,
+    U2.Last_Name AS Lister_LastName,
+    P.Property_ID,
+    P.Address_Line_1,
+    P.City,
+    P.State,
+    P.Zipcode,
+    R.Rating,
+    R.Comment
+FROM
+    Review R
+INNER JOIN
+    [User] U ON R.Customer_ID = U.User_ID
+INNER JOIN
+    [User] U2 ON R.Lister_ID = U2.User_ID
+INNER JOIN
+    Property P ON R.Property_ID = P.Property_ID;
+
+	select * from Customer_Reviews
+
+
+
+	--Properties Available for Selling
+
+	CREATE VIEW Properties_For_Sale AS
+SELECT
+    P.Property_ID,
+    P.Address_Line_1,
+    P.Address_Line_2,
+    P.City,
+    P.State,
+    P.Zipcode,
+    PD.Bedroom,
+    PD.Bathroom,
+    PD.Area_Size,
+    S.Asking_Price
+FROM
+    Property P
+INNER JOIN
+    Property_Detail PD ON P.Property_ID = PD.Property_ID
+INNER JOIN
+    Listing L ON P.Property_ID = L.Property_ID
+INNER JOIN
+    Sell S ON L.Listing_ID = S.Sell_ID
+WHERE
+    L.Listing_Type = 'S' AND
+    L.Listing_Status = 'Active';
+
+	Select * from Properties_For_Sale
+
+
+
+
+
