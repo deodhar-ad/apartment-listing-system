a7fac687dd17b148c6634080b063d4f32c1b56b0
drashti-NEU <bhingradiya.d@northeastern.edu>
2024-03-25 18:13:40 -0400

    Trigger Query

--- SQLFiles/Triggers.sql
+++ SQLFiles/Triggers.sql
@@ -1,4 +1,31 @@
--- 1.  trigger that automatically updates the Active_Properties column in the Lister table whenever a new property is inserted or deleted in the Property table.
+-- 1. Trigger that automatically updates the Active_Properties column in the Lister table whenever 
+--    a new property is inserted or deleted in the Property table by that Lister_ID.
+CREATE TRIGGER UpdateActiveProperties
+ON Property
+AFTER INSERT, DELETE
+AS
+BEGIN
+    -- Update Active_Properties for each affected Lister due to INSERT operation
+    UPDATE L
+    SET Active_Properties = (
+        SELECT COUNT(*) 
+        FROM Property P 
+        WHERE P.Lister_ID = L.Lister_ID
+    )
+    FROM Lister L
+    INNER JOIN inserted I ON L.Lister_ID = I.Lister_ID;
+
+    -- Update Active_Properties for each affected Lister due to DELETE operation
+    UPDATE L
+    SET Active_Properties = (
+        SELECT COUNT(*) 
+        FROM Property P 
+        WHERE P.Lister_ID = L.Lister_ID
+    )
+    FROM Lister L
+    INNER JOIN deleted D ON L.Lister_ID = D.Lister_ID;
+
+END;
 
 -- 2. Trigger to validate phone number is of 10 digits.
 
